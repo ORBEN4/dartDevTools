f6b5e8731b26207f73760ee2764ed5c0f93ed537
ORBEN4 <orbenshloosh@gmail.com>
2021-08-09 11:48:09 +0300

    automatic commit

--- credentials.bat
+++
@@ -1,8 +0,0 @@
-set username="ORBEN4"
-set password="or383005"
-set email="orbenshloosh@gmail.com"
-
-
-git config --global user.name %username%
-git config --global user.password %password%
-git config --global user.email %email%
--- save.bat
+++
@@ -1,5 +0,0 @@
-set project_name="‚€‰„"
-cd %project_name%
-git add .
-git commit -m "automatic commit"
-git push origin main
