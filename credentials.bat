31a4ff1f623fe1003c9e1f513205b7fda07c57d1
ORBEN4 <orbenshloosh@gmail.com>
2021-08-09 11:45:49 +0300

    automatic commit

---
+++ credentials.bat
@@ -0,0 +1,8 @@
+set username="ORBEN4"
+set password="AASSDD123"
+set email="orbenshloosh@gmail.com"
+
+
+git config --global user.name %username%
+git config --global user.password %password%
+git config --global user.email %email%
